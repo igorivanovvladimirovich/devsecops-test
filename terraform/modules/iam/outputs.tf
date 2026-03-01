d91cf3e3a110209bb354c22a4fcb70a863665bd7
ihor.ivanov <ihor.ivanov@danfoss.com>
2026-03-01 16:59:52 +0200

    feat: debug pipelines

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -1,4 +1,6 @@
 output "github_actions_sa_email" { value = google_service_account.github_actions.email }
 output "trivy_operator_sa_email" { value = google_service_account.trivy_operator.email }
 output "cloud_functions_sa_email" { value = google_service_account.cloud_functions.email }
-output "workload_identity_provider" { value = google_iam_workload_identity_pool_provider.github.name }
+output "workload_identity_provider" {
+  value = google_iam_workload_identity_pool_provider.github.name
+}
